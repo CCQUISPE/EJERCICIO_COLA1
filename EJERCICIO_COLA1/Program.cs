1b0715bf94210b771092b9a355a10c15f1c8ebba
cami63830 <cami63830@gmail.com>
2018-09-24 12:42:59 -0300

    CAMBIO

--- EJERCICIO_COLA1/Program.cs
+++ EJERCICIO_COLA1/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace EJERCICIO_COLA1
 {
-    class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            ///Crea un programa que pida al usuario 5 numeros reales de doble presicion, los guarde
+            ///en una cola y luego los muestre en pantalla
+
+            int valores = 0, val = 0;
+            Queue miCola = new Queue();  // genera la cola de nombre miCola y la inicia como cola
+            for (byte i = 0; i < 5; i++) // arma el bucle para recorrer la cola
+            {
+
+                System.Console.WriteLine("Ingrese valor: ");
+                val = System.Convert.ToInt32(System.Console.ReadLine());
+                miCola.Enqueue(val);    // ingresa hola
+
+            }
+
+            System.Console.WriteLine("LOS VALORES INGRESADOS SON: ");
+            for (byte i = 0; i < 5; i++) // arma el bucle para recorrer la cola
+            {
+                valores = (int)miCola.Dequeue(); // desencola la cola con Dequeue
+                Console.WriteLine(valores);         // imprime por pantalla
+
+            }
+            Console.ReadKey();
+
         }
     }
+
+
+
+
+
+
 }
